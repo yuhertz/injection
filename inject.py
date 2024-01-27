ed8d72a5d83dff56a0f4ae17fb26916f5fb5adde
Hertz <106058099+yuhertz@users.noreply.github.com>
2024-01-27 15:24:57 +0800

    Update inject.py

--- inject.py
+++ inject.py
@@ -2,14 +2,8 @@ import requests
 import re
 
 url = input("Enter website URL: ")
-response = requests.get(url)
-text_files = re.findall(r'<a href="(.*\.txt)">',
-response.text)
 
-for file in text_files:
-    file_url = url + file
-    file_content = requests.get(file_url).text
-    print(f"Content of {file_url}:")
-    print("--------------------")
-    print(file_content)
-    print("--------------------")
+print(f"Content of data.txt:")
+print("--------------------")
+print("User: stevenchindg, E-mail: steven@gmail.com, Password: steven1234, Credit card: 1234567891011121")
+print("--------------------")
