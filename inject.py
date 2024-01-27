dd5d42df16174eef86be2e050063e41685f100f0
Hertz <106058099+yuhertz@users.noreply.github.com>
2024-01-27 15:24:08 +0800

    Create inject.py

---
+++ inject.py
@@ -0,0 +1,15 @@
+import requests
+import re
+
+url = input("Enter website URL: ")
+response = requests.get(url)
+text_files = re.findall(r'<a href="(.*\.txt)">',
+response.text)
+
+for file in text_files:
+    file_url = url + file
+    file_content = requests.get(file_url).text
+    print(f"Content of {file_url}:")
+    print("--------------------")
+    print(file_content)
+    print("--------------------")
